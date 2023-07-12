7eca8403efeaab4076ef506fb86b082818cd121d
Lisa <>
2023-07-12 21:23:47 +0200

    canvas setup

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ import { createCanvas } from './modules/canvas.js';
 
 document.body.style.width = '100vw';
 document.body.style.height = '100vh';
-createCanvas('cnv', document.body, 1, 2/3, 400);
+const [cnv, ctx] = createCanvas('cnv', document.body, 2, 3, 1);
 console.log("canvas created");
+console.log(cnv.width/cnv.height, 2/3);
--- modules/canvas.js
+++ modules/canvas.js
@@ -3,24 +3,39 @@
  * 
  * @param {string} id Id for canvas
  * @param {HTMLElement} parent HTML container element 
- * @param {number} scale Scale factor of canvas, (0; 1]
- * @param {number} ratio Aspect ratio of canvas
+ * @param {number} aspW Width
+ * @param {number} aspH Height
+ * @param {number} scale Scale factor of canvas; (0; 1]; 1 is max screen fit
  * 
  * @description Create canvas element in parent with specified scale factor and aspect ratio.
  * 
  * @returns Canvas is appended as child of parent. Return 2d context of canvas.
  */
-function createCanvas(id, parent, scale = 0.9, ratio = 1/1) {
-    // create and scale
+function createCanvas(id, parentElem, aspW = 2, aspH = 3, scale = 1) {
+    // canvas element
     let cnv = document.createElement('canvas');
     cnv.id = id;
-    cnv.width = cnv.height = Math.min(parent.offsetWidth, parent.offsetHeight);
-    cnv.width *= ratio;
-    let sc = Math.max(cnv.width/ parent.offsetWidth, cnv.height / parent.offsetHeight);
-    cnv.width *= (1 + (1 - sc))*scale;
-    cnv.height *= (1 + (1 - sc))*scale;
-    parent.appendChild(cnv);
-    return cnv.getContext('2d');
+    // parent dim
+    const parentW = parentElem.clientWidth, parentH = parentElem.clientHeight;
+    const parentAsp = parentW / parentH;
+    // element dim
+    const cnvAsp = aspW / aspH;
+    // determine new dim
+    let cnvWidth, cnvHeight;
+    if (parentAsp > cnvAsp) {
+    cnvHeight = parentH; 
+    cnvWidth = cnvHeight * cnvAsp;
+    }
+    else {
+    cnvWidth = parentW; 
+    cnvHeight = cnvWidth / cnvAsp;
+    }
+    //
+    cnv.width = cnvWidth * scale;
+    cnv.height = cnvHeight * scale;
+    
+    parentElem.appendChild(cnv);
+    return [cnv, cnv.getContext('2d')];
 }
 
 
